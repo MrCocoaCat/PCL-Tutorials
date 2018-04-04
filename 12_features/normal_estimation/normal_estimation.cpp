36da25e91b9f8c2d25ea902a7b80713dba1370e5
MrCocoaCat <liyubo_liyubo@163.com>
2018-04-04 08:36:58 +0800

    add

--- 12_features/normal_estimation/normal_estimation.cpp
+++ 12_features/normal_estimation/normal_estimation.cpp
@@ -5,35 +5,39 @@
 #include <pcl/point_types.h>
 #include <pcl/features/normal_3d.h>
 
-int
-main ()
- {
-//加载点云
-pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
-pcl::io::loadPCDFile ("table_scene_lms400.pcd", *cloud);
-//估计法线
-pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> ne;
-ne.setInputCloud (cloud);
-//创建一个空的kdtree对象，并把它传递给法线估计对象
-//基于给出的输入数据集，kdtree将被建立
-pcl::search::KdTree<pcl::PointXYZ>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZ> ());
-ne.setSearchMethod (tree);
-//输出数据集
-pcl::PointCloud<pcl::Normal>::Ptr cloud_normals (new pcl::PointCloud<pcl::Normal>);
-//使用半径在查询点周围3厘米范围内的所有邻元素
-ne.setRadiusSearch (0.03);
-//计算特征值
-ne.compute (*cloud_normals);
-// cloud_normals->points.size ()应该与input cloud_downsampled->points.size ()有相同尺寸
-//法线可视化
-pcl::visualization::PCLVisualizer viewer("PCL Viewer");
-viewer.setBackgroundColor (0.0, 0.0, 0.0);
-viewer.addPointCloudNormals<pcl::PointXYZ,pcl::Normal>(cloud, cloud_normals);
-
-while (!viewer.wasStopped ())
+int main ()
 {
-     viewer.spinOnce ();
-}
+	//加载点云
+	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
+	pcl::io::loadPCDFile ("table_scene_lms400.pcd", *cloud);
+	//估计法线
+	pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> ne;
+	ne.setInputCloud (cloud);
+	//创建一个空的kdtree对象，并把它传递给法线估计对象
+	//基于给出的输入数据集，kdtree将被建立
+	//创建Kdtree对象
+	pcl::search::KdTree<pcl::PointXYZ>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZ> ());
+	//设置收缩方式
+	ne.setSearchMethod (tree);
+	//输出数据集
+	pcl::PointCloud<pcl::Normal>::Ptr cloud_normals (new pcl::PointCloud<pcl::Normal>);
+	
+	//设置收缩半径，使用半径在查询点周围3厘米范围内的所有邻元素 
+	ne.setRadiusSearch (0.03);
+	//计算特征值
+	ne.compute (*cloud_normals);
+	
+	// cloud_normals->points.size ()应该与input cloud_downsampled->points.size ()有相同尺寸
+	
+	//法线可视化
+	pcl::visualization::PCLVisualizer viewer("PCL Viewer");
+	viewer.setBackgroundColor (0.0, 0.0, 0.0);
+	viewer.addPointCloudNormals<pcl::PointXYZ,pcl::Normal>(cloud, cloud_normals);
 
-return 0;
+	while (!viewer.wasStopped ())
+	{
+	 viewer.spinOnce ();
+	}
+
+	return 0;
 }
--- nefu/readme.md
+++ nefu/readme.md
@@ -4,3 +4,6 @@
 
 ###滤波之后图像
  ![Alt text](./pic/filtered.png)
+
+
+org:518400 after vol 26409
